6d87058bf188c78115271091b8fab2bf9ad8bc1d
Esther White <esthersoftwaredev@gmail.com>
2025-02-16 13:09:22 +0200

    navbar

--- next-online-store/app/checkout/page.tsx
+++ next-online-store/app/checkout/page.tsx
@@ -1,6 +1,8 @@
 export default function CheckoutPage() {
-	<div className='container mx-auto p-8 flex flex-col items-center justify-center h-screen'>
-		<h1 className='text-4xl font-bold mb-4'>Check Out</h1>
-		<p className='text-gray-600 text-center'>This feature is coming soon!</p>
-	</div>;
+  return (
+    <div className="container mx-auto p-8 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Check Out</h1>
+      <p className="text-gray-600 text-center">This feature is coming soon!</p>
+    </div>
+  );
 }
