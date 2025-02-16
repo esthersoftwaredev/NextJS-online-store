a4b288150725efe494733c0c1b9850187916ac69
Esther White <esthersoftwaredev@gmail.com>
2025-02-16 11:13:44 +0200

    linking images

--- next-online-store/app/ProductsList.tsx
+++ next-online-store/app/ProductsList.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Product } from "./product-data";
 
 export default function ProductsList({ products }: { products: Product[] }) {
 	return (
 		<div>
 			{products.map((product) => (
-				<div key={product.id}>
+				<Link key={product.id} href="/product-details">
           <Image
             src={'/' + product.imageUrl}
             alt={product.name}
@@ -14,7 +15,7 @@ export default function ProductsList({ products }: { products: Product[] }) {
           />
 					<h2>{product.name}</h2>
 					<p>${product.price}</p>
-				</div>
+				</Link>
 			))}
 		</div>
 	);
