94e7747d93041977ebe9e43e9db9a6e1ed5d157c
Esther White <esthersoftwaredev@gmail.com>
2025-02-16 20:03:27 +0200

    deleting an item from cart

--- next-online-store/app/api/users/[id]/cart/route.ts
+++ next-online-store/app/api/users/[id]/cart/route.ts
@@ -65,3 +65,23 @@ export async function POST(
 		},
 	});
 }
+
+export async function DELETE(	
+	request: NextRequest,
+	{ params }: { params: Params }
+) {
+	const userId = params.id;
+	const body: CartBody = await request.json();
+	const productId = body.productId;
+
+	carts[userId] = carts[userId] ? carts[userId].filter((id) => id !== productId) : [];
+	const cartProducts = carts[userId].map((id) => products.find((product) => product.id === id)!);
+
+
+	return new Response(JSON.stringify(cartProducts), {
+		status: 202,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+}
